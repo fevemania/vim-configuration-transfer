5e1737ebef6c1c847736e4c0070ba750cf664ee4
fevemania <brent12052003@gmail.com>
2017-04-02 01:26:56 +0800

    debug fatal error

--- script.py
+++ script.py
@@ -9,18 +9,21 @@ def check_module_exists(module_name):
     if module_name not in result.stdout.decode('utf-8'):  # so we should decode it to extract proper string
         subprocess.run(['pip3', 'install', module_name])
 
-# check_file_exist, if exist throw except_action, else do regular action
-def check_file_exist(filepath, action):
+# check_file_exist, if exist throw except_action, else do copy file action
+def check_file_exist(filepath, action, filename, new_path):
     if os.path.isfile(filepath):
         print(filepath, 'already exist, please check it')
     else:
-        action()
+        action(filename, new_path)
 
-def check_dir_exist(dirpath, action):
+def check_dir_exist(dirpath, action, current_path, new_path):
     if os.path.isdir(dirpath):
         print(dirpath, 'already exist, please check it')
     else:
-        action()
+        action(current_path, new_path)
+
+def rename(current_path, new_path):
+    os.rename(current_path, new_path)
 
 if __name__ == '__main__':
     # Step1. pip3 install GitPython, if not exist.
@@ -31,15 +34,15 @@ if __name__ == '__main__':
     
     # Step2. mv vim_config .vim
     dot_vim_path = home_dir + '/.vim'
-    check_dir_exist(dot_vim_path, os.rename(current_path, dot_vim_path))
+    check_dir_exist(dot_vim_path, os.rename, current_path, dot_vim_path) 
     
     # Step3. cp .vim/.vimrc ~
     vimrc_path = home_dir + '/.vimrc'
-    check_file_exist(vimrc_path, copyfile('.vimrc', vimrc_path)) 
+    check_file_exist(vimrc_path, copyfile, '.vimrc', vimrc_path) 
     
     # Step4. cp copy.py ~ (for auto_copy .vimrc to .vim whenever change)
     copy_py_file = home_dir + '/copy.py'
-    check_file_exist(copy_py_file, copyfile('copy.py', copy_py_file))
+    check_file_exist(copy_py_file, copyfile, 'copy.py', copy_py_file)
     
     # Step5. download for Vundle
     subprocess.run(['python3', 'auto_download.py']) 
