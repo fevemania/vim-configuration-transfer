c6829d87f4edc4478118cb0f25a4894ca5446780
fevemania <brent12052003@gmail.com>
2017-04-02 00:27:01 +0800

    beta test3: auto_download.py

--- auto_download.py
+++ auto_download.py
@@ -3,18 +3,19 @@ import os
 from pathlib import Path 
 from os.path import expanduser
 
-def auto_download(dir_name):
+def auto_download(filename):
     HOME = os.path.expanduser('~')
     bundle_dir = os.path.join(HOME, '.vim/bundle')
     if not os.path.exists(bundle_dir):
         os.makedirs(bundle_dir)
-    vundle_path = Path(os.path.join(HOME, dir_name))
+
+    vundle_path = Path(os.path.join(HOME, filename))
 
     # Check if Vundle.vim exist to avoid repeatly download 
     if vundle_path.is_dir():
     	print(dir_name, " is alreadly in ", vundle_path)
     else:
-    	Repo.clone_from("git@github.com:VundleVim/Vundle.vim.git", dir_name,branch="master")
+    	Repo.clone_from("https://github.com/VundleVim/Vundle.vim.git", vundle_path, branch="master")
 
 if __name__ == "__main__":
-	auto_download('.vim/bundle/Vundle.vim')
+	auto_download('Vundle.vim')
